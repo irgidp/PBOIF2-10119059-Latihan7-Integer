a0f34aa8f3e365aa062305f0990be459d9dd828f
irgidp <irgijak@gmail.com>
2020-10-15 22:06:17 +0700

    Latihan 7

---
+++ PBOIF210119059Latihan7Integer.java
@@ -0,0 +1,33 @@
+/* 
+* @author
+ * NAMA        : Irgi Dwiputra
+ * KELAS       : IF-2
+ * NIM         : 10119059
+ * Deskripsi   : Program ini berisi program untuk menampilkan penulisan integer
+ */
+package pboif2.pkg10119059.latihan7.integer;
+
+public class PBOIF210119059Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+        
+    }
+    
+}
